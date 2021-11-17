648b270e031c32423f77b7672ecdffa0727e4b82
Iskander Saifullin <cjiebuh@list.ru>
2021-11-17 20:17:15 +0300

    division by zero fix

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -9,6 +9,7 @@ module Exercise
         rating_ar = array.select{|film| film["country"].to_s.count(",") >= 1}
                          .select{|film| film["rating_kinopoisk"].to_f > 0.0}
                          .map{|film| film["rating_kinopoisk"].to_f}
+        return 0 if rating_ar.count == 0
         rating_sum = rating_ar.reduce(:+)
         return rating_sum / rating_ar.count
       end
