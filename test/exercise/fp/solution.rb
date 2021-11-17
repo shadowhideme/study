0b2122df93fa954930e12d34e7ff6f7ff8e07fd6
Iskander Saifullin <cjiebuh@list.ru>
2021-11-18 00:51:38 +0300

    after rubocop

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,18 +5,19 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        rating_ar = array.select{|film| film["country"].to_s.count(",") >= 1}
-                         .select{|film| film["rating_kinopoisk"].to_f > 0.0}
-                         .map{|film| film["rating_kinopoisk"].to_f}
-        return 0 if rating_ar.count == 0
-        rating_sum = rating_ar.reduce(:+)
-        return rating_sum / rating_ar.count
+        rating_array = array.select { |film| film['country'].to_s.count(',') >= 1 }
+                            .select { |film| film['rating_kinopoisk'].to_f > 0.0 }
+                            .map { |film| film['rating_kinopoisk'].to_f }
+        return 0 if rating_array.count.zero?
+
+        rating_sum = rating_array.reduce(:+)
+        rating_sum / rating_array.count
       end
 
       def chars_count(films, threshold)
-        return films.select{|film| film["rating_kinopoisk"].to_f > threshold}
-                    .map{|film| film["name"].to_s.count("и")}
-                    .reduce(:+)
+        films.select { |film| film['rating_kinopoisk'].to_f > threshold }
+             .map { |film| film['name'].to_s.count('и') }
+             .reduce(:+)
       end
     end
   end
