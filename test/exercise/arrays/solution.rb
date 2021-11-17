e5cdea693e6166b2803cd5dbb4b524f39da5965e
Iskander Saifullin <cjiebuh@list.ru>
2021-11-18 00:54:33 +0300

    after rubocop

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,12 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        return array if array.count == 0
+        return array if array.count.zero?
+
         max_el = find_max(array)
         array.map do |el|
-           if el > 0
-            max_el 
+          if el.positive?
+            max_el
           else
             el
           end
@@ -16,27 +17,28 @@ module Exercise
       def search(array, query)
         index = bin_search(array, query, -1, array.count)
         if array[index] == query
-	        return index
-	      else
-	        return -1 #if not found
-	      end
+          index
+        else
+          -1 # if not found
+        end
       end
 
-      def find_max(array) 
+      def find_max(array)
         max = array[0]
-        array.each{| el| max = el if max < el }
+        array.each { |el| max = el if max < el }
         max
       end
-      
-      def bin_search(array, el, left_index, right_index)
-        return right_index if (left_index >= right_index - 1)
+
+      def bin_search(array, element, left_index, right_index)
+        return right_index if left_index >= right_index - 1
+
         mid_index = (left_index + right_index) / 2
-        if array[mid_index] < el
+        if array[mid_index] < element
           left_index = mid_index
-        else 
+        else
           right_index = mid_index
         end
-        bin_search(array, el, left_index, right_index)
+        bin_search(array, element, left_index, right_index)
       end
     end
   end
