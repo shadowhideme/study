d55e529eec2ff7cfe0b57b539a905ecad38976ba
Iskander Saifullin <cjiebuh@list.ru>
2021-11-17 22:42:53 +0300

    deleted comment

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,6 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        # nil.to_s == "", nil.to_f == 0.0
         rating_ar = array.select{|film| film["country"].to_s.count(",") >= 1}
                          .select{|film| film["rating_kinopoisk"].to_f > 0.0}
                          .map{|film| film["rating_kinopoisk"].to_f}
